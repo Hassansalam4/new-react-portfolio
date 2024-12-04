07e65efdf37d6f32149f4567f12c169b103cc80d
Hassansalam4 <atuman4@gmail.com>
2024-12-03 19:25:52 -0500

    added basic structure of each page

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -1,22 +1,31 @@
 export default function Blog() {
   return (
     <div>
-      <h1>Blog Page</h1>
+      <h1>Projects Page</h1>
       <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
+        Here are some of the projects I have worked on.
       </p>
+
+      <div className="card">
+        <h3>Project 1</h3>
+        <p>Project 1 description</p>
+        // Add an image of the project
+        <a href="example.com">Link to project 1</a>
+      </div>
+      <div className="card">
+        <h3>Project 2</h3>
+        <p>Project 2 description</p>
+        // Add an image of the project
+        <a href="example.com">Link to project 2</a>
+      </div>
+
+      <div className="card">
+        <h3>Project 3</h3>
+        <p>Project 3 description</p>
+        // Add an image of the project
+        <a href="example.com">Link to project 3</a>
+      </div>
+
     </div>
   );
 }
